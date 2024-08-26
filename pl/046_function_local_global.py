076d690b3b2643a23e06ae37b6f973730599f92e
QiWang <Qi.Wang@tracetronic.de>
2024-08-26 16:05:06 +0200

    gf

--- pl/046_function_local_global.py
+++ pl/046_function_local_global.py
@@ -4,3 +4,22 @@
 # @File: 046_function_local_global
 # @Project: python_unittest
 
+# def f1():
+#     # local, in function
+#     a = 1
+#     print(a)
+#
+# f1()
+# # print(a) # error
+
+a = 1
+print(a)
+
+
+def f1():
+    print(a)
+
+
+f1()
+
+# local is preferred if possible
---
+++ pl/047_open and_close_file.py
@@ -0,0 +1,23 @@
+# _*_ coding: utf-8 _*_
+# @Time: 26.08.2024 15:47
+# @Author: Qi Wang
+# @File: 047_open and_close_file
+# @Project: python_unittest
+
+'''# open(filepath, mode)
+# mode: w: writable
+#       r: readable
+open('text.txt', 'w')'''
+
+# open file
+# fp = open('text.txt', 'w')
+# fp.write('hello world')
+
+# folder can't be created automatically if it doesn't exist, it must be done manually
+fp = open('demo/text.txt', 'w')
+fp.write('hello world')
+
+# close file
+fp = open('demo/a.txt','w')
+fp.write('hello world')
+# fp.close()
