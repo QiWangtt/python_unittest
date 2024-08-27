0200876fa9d41d2e04a0ed6aa6c2993cc92a87a7
QiWang <Qi.Wang@tracetronic.de>
2024-08-27 09:09:10 +0200

    zgt

--- pl/048_file_write_read.py
+++ pl/048_file_write_read.py
@@ -4,14 +4,26 @@
 # @File: 048_file_write_read
 # @Project: python_unittest
 # If file exists, it will be cleared at first, then write new info
-fp = open('test.txt', 'w')
-fp.write('hello world, I am here\n' * 5)
-fp.close()
+# fp = open('test.txt', 'w')
+# fp.write('hello world, I am here\n' * 5)
+# fp.close()
+#
+#
+# # appending, mode: a
+# fp = open('test.txt', 'a')
+# fp.write('hello world, I am here\n' * 5)
+# fp.close()
 
+# read
+fp = open('test.txt', 'r')
+# default, read efficiency is too low
+# content = fp.read()
+# print(content)
 
-# appending, mode: a
-fp = open('test.txt', 'a')
-fp.write('hello world, I am here\n' * 5)
-fp.close()
+# readline is higher
+# contents = fp.readline()
+# print(contents)
 
-# p48 noch nicht fertig
+# readlines high, but output is in form of list
+contents = fp.readlines()
+print(contents)
---
+++ pl/049_jason.py
@@ -0,0 +1,8 @@
+# _*_ coding: utf-8 _*_
+# @Time: 27.08.2024 09:06
+# @Author: Qi Wang
+# @File: 049_jason
+# @Project: python_unittest
+fp = open('test.txt', 'w')
+fp.write('hello world')
+fp.close()
