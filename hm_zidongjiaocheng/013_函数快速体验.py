3f86261a7e8307ee847b6ec7e3e0f4f3a3f5e2f9
QiWang <Qi.Wang@tracetronic.de>
2024-08-28 13:20:59 +0200

    k

--- hm_zidongjiaocheng/013_函数快速体验.py
+++ hm_zidongjiaocheng/013_函数快速体验.py
@@ -10,7 +10,6 @@ def login():
     print('3. Token')
     print('4. login')
     print('-' * 30)
-# tzhrehn
 
 login()
 
---
+++ hm_zidongjiaocheng/08_交换两个变量的值.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+# @Time: 28.08.2024 11:16
+# @Author: Qi Wang
+# @File: 08_交换两个变量的值
+# @Project: python_unittest
+
+#
+a = 10
+b = 20
+c = a
+a = b
+b = c
