e579a82480e111a07d0bea88ff3321d8e7b7decf
QiWang <Qi.Wang@tracetronic.de>
2024-08-29 09:31:52 +0200

    o

---
+++ hm_zidongjiaocheng/113_sum.py
@@ -0,0 +1,6 @@
+# _*_ coding: utf-8 _*_
+# @Time: 29.08.2024 09:31
+# @Author: Qi Wang
+# @File: 113_sum
+# @Project: python_unittest
+# Quelle: https://www.youtube.com/watch?v=aHRb8RoX9vI&list=PLFbd8KZNbe-_PWBT_L7V6fNYJOgER0ZlR&index=113
